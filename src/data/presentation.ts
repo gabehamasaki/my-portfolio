9d1eca823c6357cf1bb7c0230582a57d8b26c85b
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-03-08 11:41:02 -0300

    update title page and description

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -18,7 +18,7 @@ const presentation: Presentation = {
   title: "Hi, I’m Gabriel Hamasaki 👋",
   // profile: "/profile.webp",
   description:
-    `Welcome, i'm a *Brazilian Systems Analyst* with over *2 years* of web experience. I am currently working with *Laravel* and *Vuejs* in ${exata} Outside of work i am learning *nextjs* with *typescript* and *tailwindcss* to improve my skills. I am also a *otaku* and *gamer* in my free time. I am always looking for new challenges and opportunities to grow.`,
+    `Welcome, i'm a *Brazilian Software Engineer* with over *2 years* of web experience. I am currently working with *Laravel* and *Vuejs* in ${exata} Outside of work i am learning *nextjs* with *typescript* and *tailwindcss* to improve my skills. I am also a *otaku* and *gamer* in my free time. I am always looking for new challenges and opportunities to grow.`,
   socials: [
     {
       label: "LinkedIn",
