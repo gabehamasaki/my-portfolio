ef5dd209985bcea9c2c55824479c86e0fb9985db
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-03-08 11:33:54 -0300

    :tada: Start engines

---
+++ src/content/posts/01-untitled.md
@@ -0,0 +1,10 @@
+--- 
+title: "Empty post"
+publishedAt: 2024-03-08
+description: "Empty post"
+slug: "empty-post"
+isPublish: false
+isDraft: false
+---
+
+## Hello World
---
+++ src/data/presentation.ts
@@ -0,0 +1,38 @@
+type Social = {
+  label: string;
+  link: string;
+};
+
+type Presentation = {
+  mail: string;
+  title: string;
+  description: string;
+  socials: Social[];
+  profile?: string;
+};
+
+const exata = "<a href='https://www.exata.it/en/' target='_blank' class='hover:underline'>*Exata Tech*</a>"
+
+const presentation: Presentation = {
+  mail: "gabrielhamasaki82@outlook.com",
+  title: "Hi, I’m Gabriel Hamasaki 👋",
+  // profile: "/profile.webp",
+  description:
+    `Welcome, i'm a *Brazilian Systems Analyst* with over *2 years* of web experience. I am currently working with *Laravel* and *Vuejs* in ${exata} Outside of work i am learning *nextjs* with *typescript* and *tailwindcss* to improve my skills. I am also a *otaku* and *gamer* in my free time. I am always looking for new challenges and opportunities to grow.`,
+  socials: [
+    {
+      label: "LinkedIn",
+      link: "https://linkedin.com/in/gabriel-hamasaki/",
+    },
+    {
+      label: "Github",
+      link: "https://github.com/gabehamasaki",
+    },
+    {
+      label: 'Repository',
+      link: "https://github.com/gabehamasaki/my-portfolio"
+    }
+  ],
+};
+
+export default presentation;
