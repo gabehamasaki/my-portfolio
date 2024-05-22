723f78d7fef8026f9ade0929e5c6c598380d86b0
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-05-22 16:36:00 -0300

    feat(presentation): add experience function

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -12,15 +12,15 @@ type Presentation = {
   profile?: string;
 };
 
-const exata = "<a href='https://www.exata.it/en/' target='_blank' class='hover:underline'>*Exata Technology*</a>"
-
+const exata =
+  "<a href='https://www.exata.it/en/' target='_blank' class='hover:underline'>*Exata Technology*</a>";
+const yearsOfExperience = new Date().getFullYear() - 2019;
 const presentation: Presentation = {
   name: "Gabriel Hamasaki",
   mail: "gabrielhamasaki82@outlook.com",
   title: "Hi, I’m Gabriel Hamasaki 👋",
   // profile: "/profile.webp",
-  description:
-    `Welcome! I'm a *Brazilian Software Engineer* with over *2 years* of experience in *web development*. Currently, I'm working with *Laravel* and *Vue.js* at ${exata}. Outside of work, I'm enhancing my skills by learning *Next.js* with *TypeScript* and *Tailwind CSS*. In my free time, I enjoy *anime* (as an otaku) and *video games*. I'm always eager to take on new *challenges* and *opportunities* that will foster *my growth*.`,
+  description: `Welcome! I'm a *Brazilian Software Engineer* with over *${yearsOfExperience} years* of experience in *web development*. Currently, I'm working with *Laravel* and *Vue.js* at ${exata}. Outside of work, I'm enhancing my skills by learning *Next.js* with *TypeScript* and *Tailwind CSS*. In my free time, I enjoy *anime* (as an otaku) and *video games*. I'm always eager to take on new *challenges* and *opportunities* that will foster *my growth*.`,
   socials: [
     {
       label: "LinkedIn",
@@ -31,9 +31,9 @@ const presentation: Presentation = {
       link: "https://github.com/gabehamasaki",
     },
     {
-      label: 'Repository',
-      link: "https://github.com/gabehamasaki/my-portfolio"
-    }
+      label: "Repository",
+      link: "https://github.com/gabehamasaki/my-portfolio",
+    },
   ],
 };
 
