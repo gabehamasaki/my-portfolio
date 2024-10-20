90bb342cb660bd75c682ac9297ef9c7f03e2a5f6
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-10-20 01:04:30 -0300

    feat: add orders project to the projects list

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,12 +9,20 @@ export type Project = {
 
 const projects: Project[] = [
   {
+    title: "Orders API",
+    name: "orders",
+    techs: ["Golang", "GRPC", "Postgres", "Docker", "Redis", "Gin-Gonic", "SQLC", "Protobuf", "buf"],
+    link: "https://github.com/gabehamasaki/orders",
+    isComingSoon: false,
+  },
+  {
     title: "Polls API",
     name: "polls",
     techs: ["Fastify", "TypeScript", "Drizzle", "Websocket", "Redis", "Zod"],
     link: "https://github.com/gabehamasaki/polls",
     isComingSoon: false,
   },
+
 ];
 
 export default projects;
